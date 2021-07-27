c03d3a509be874aaf292a36e9833084351f189f4
djm2x <dj-m2x@hotmail.com>
2021-07-27 01:02:42 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// https://dev.to/angular/setup-tailwindcss-in-angular-the-easy-way-1i5l
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'),require('@tailwindcss/typography')],
+}
